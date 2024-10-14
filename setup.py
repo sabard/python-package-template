bf93688ea1dec89a9b7e8cf1fad54acec3a6dfbd
Sabar Dasgupta <s@bardasgupta.com>
2024-10-14 15:33:21 -0400

    environment fiddling 9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as fh:
 
 setup(
     name='python_package_template',
-    version='0.0.1',
+    version='0.0.1-test',
     license='AGPLv3',
     description='Template for creating and generic Python packages',
     long_description=long_description,
